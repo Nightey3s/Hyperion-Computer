4113de7b70aa837949fa3e96799a4d819b8c506d
Brian <briantham1@gmail.com>
2019-01-29 22:14:18 +0800

    Added Javascript
    Javascript on each page to hide certain elements of nav bar when hovering on product button.
    
    Changed product dropdown to 3 by 2 instead of displaying all in one line
    
    Printing the form data is implemented

---
+++ printForm.js
@@ -0,0 +1,16 @@
+function getFormData()
+{var formData = location.search;
+formData = formData.substring(1, formData.length);
+while (formData.indexOf("+") !== -1) {
+   formData = formData.replace("+", " ");
+}
+formData = decodeURIComponent(formData);
+var formArray = formData.split("&");
+for (var i = 0; i < formArray.length; i++) {
+   document.write("<p>" + formArray[i] + "</p>");
+   if(i === 1){
+     document.write("<br>");
+   }
+}
+}
+getFormData();
