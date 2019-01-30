8de80ff9d641ae8ab80fddd487b55b07fb17b2ef
Brian <briantham1@gmail.com>
2019-01-30 16:37:47 +0800

    Added JQuery for FAQ
    JQuery transition to open and close sections of FAQ, with a 550ms time to open/close
    
    Showclick.js is the JQuery file that handles the clicks

---
+++ showonclick.js
@@ -0,0 +1,43 @@
+$("#q1").click(function(){
+  $("#ans1").toggle(550);
+})
+
+$("#q2").click(function(){
+  $("#ans2").toggle(550);
+})
+
+$("#q3").click(function(){
+  $("#ans3").toggle(550);
+})
+
+$("#q4").click(function(){
+  $("#ans4").toggle(550);
+})
+
+$("#q5").click(function(){
+  $("#ans5").toggle(550);
+})
+
+$("#q6").click(function(){
+  $("#ans6").toggle(550);
+})
+
+$("#q7").click(function(){
+  $("#ans7").toggle(550);
+})
+
+$("#q8").click(function(){
+  $("#ans8").toggle(550);
+})
+
+$("#q9").click(function(){
+  $("#ans9").toggle(550);
+})
+
+$("#q10").click(function(){
+  $("#ans10").toggle(550);
+})
+
+$("#q11").click(function(){
+  $("#ans11").toggle(550);
+})
