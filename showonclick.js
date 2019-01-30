805323b1bbb4311cf9c54392d89e95c894fcb1d8
Brian <briantham1@gmail.com>
2019-01-30 19:09:22 +0800

    Changed BG Color & JQuery
    BG Color: #CFCFCF
    JQuery changed .toggle to .slideToggle

--- showonclick.js
+++ showonclick.js
@@ -1,43 +1,43 @@
 $("#q1").click(function(){
-  $("#ans1").toggle(700);
+  $("#ans1").slideToggle(700);
 })
 
 $("#q2").click(function(){
-  $("#ans2").toggle(700);
+  $("#ans2").slideToggle(700);
 })
 
 $("#q3").click(function(){
-  $("#ans3").toggle(700);
+  $("#ans3").slideToggle(700);
 })
 
 $("#q4").click(function(){
-  $("#ans4").toggle(700);
+  $("#ans4").slideToggle(700);
 })
 
 $("#q5").click(function(){
-  $("#ans5").toggle(700);
+  $("#ans5").slideToggle(700);
 })
 
 $("#q6").click(function(){
-  $("#ans6").toggle(700);
+  $("#ans6").slideToggle(700);
 })
 
 $("#q7").click(function(){
-  $("#ans7").toggle(700);
+  $("#ans7").slideToggle(700);
 })
 
 $("#q8").click(function(){
-  $("#ans8").toggle(700);
+  $("#ans8").slideToggle(700);
 })
 
 $("#q9").click(function(){
-  $("#ans9").toggle(700);
+  $("#ans9").slideToggle(700);
 })
 
 $("#q10").click(function(){
-  $("#ans10").toggle(700);
+  $("#ans10").slideToggle(700);
 })
 
 $("#q11").click(function(){
-  $("#ans11").toggle(700);
+  $("#ans11").slideToggle(700);
 })
