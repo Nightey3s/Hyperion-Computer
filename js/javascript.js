655fe770c438ac917ba6f39be47f3f7bc88c4e84
Brian <briantham1@gmail.com>
2019-02-11 00:59:36 +0800

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,3 @@
-  var a = document.getElementById("myTopnav");
-  var b = document.getElementById("burger")
-  if(b.addEventListener){
-    b.addEventListener("click", mobileclick);
-  }
-
-  function mobileclick(){
-    if (a.className === "topnav") {
-      a.className += " responsive";
-    } else {
-      a.className = "topnav";
-    }
-  }
-
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(this).scrollTop() > 200) {
