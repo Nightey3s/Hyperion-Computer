c2c5e031640ea4040ead1890cbc02368da372cae
Brian <briantham1@gmail.com>
2019-02-13 18:01:15 +0800

    Added customisation pages

---
+++ js/showcustomisation.js
@@ -0,0 +1,71 @@
+$("#changeprocessor").click(function(){
+  $("#changeprocessor1").slideToggle(1000);
+})
+
+$("#changeMotherboard").click(function(){
+  $("#changeMotherboard1").slideToggle(1000);
+})
+
+$("#changeRAM").click(function(){
+  $("#changeRAM1").slideToggle(1000);
+})
+
+$("#changePSU").click(function(){
+  $("#changePSU1").slideToggle(1000);
+})
+
+$("#changedrive1").click(function(){
+  $("#changedrive1-2").slideToggle(1000);
+})
+
+$("#changedrive2").click(function(){
+  $("#changedrive2-2").slideToggle(1000);
+})
+
+$("#changedrive3").click(function(){
+  $("#changedrive3-2").slideToggle(1000);
+})
+
+$("#changegpu").click(function(){
+  $("#changegpu1").slideToggle(1000);
+})
+
+$("#changeSound").click(function(){
+  $("#changeSound1").slideToggle(1000);
+})
+
+$("#changeCooling").click(function(){
+  $("#changeCooling1").slideToggle(1000);
+})
+
+$("#changeCable").click(function(){
+  $("#changeCable1").slideToggle(1000);
+})
+
+$("#changeFan").click(function(){
+  $("#changeFan1").slideToggle(1000);
+})
+
+$("#changeOS").click(function(){
+  $("#changeOS1").slideToggle(1000);
+})
+
+$("#changeOffice").click(function(){
+  $("#changeOffice1").slideToggle(1000);
+})
+
+$("#changeMonitor").click(function(){
+  $("#changeMonitor1").slideToggle(1000);
+})
+
+$("#changeKeyboard").click(function(){
+  $("#changeKeyboard1").slideToggle(1000);
+})
+
+$("#changeMouse").click(function(){
+  $("#changeMouse1").slideToggle(1000);
+})
+
+$("#changeAudio").click(function(){
+  $("#changeAudio1").slideToggle(1000);
+})
